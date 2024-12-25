381870ddb895b245f262f7348551b74bb6996c68
Windows 10 <adhambekhusinboyev@gmail.com>
2024-12-25 14:12:41 +0500

    first-commit

---
+++ web/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.static import static
+from django.urls import path
+
+from config import settings
+from .views import *
+
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('int_tir', int_tir, name='int_tir'),
+    path('lasertir', laser_tir, name='laser_tir'),
+    path('panoroma', panorama, name='panorama'),
+    path('btr', btr, name='btr'),
+    path('desant', desant, name='desant'),
+    path('dpm', dpm, name='dpm'),
+    path('mobil', mobil, name='mobil'),
+    path('pzrk', pzrk, name='pzrk'),
+    path('vr', vr, name='vr'),
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ web/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def int_tir(request):
+    return render(request, "int_tir.html")
+
+
+def laser_tir(request):
+    return render(request, "laser_tir.html")
+
+
+def panorama(request):
+    return render(request, "panarama.html")
+
+
+def btr(request):
+    return render(request, "btr.html")
+
+
+def desant(request):
+    return render(request, "desant.html")
+
+
+def dpm(request):
+    return render(request, "dpm.html")
+
+
+def mobil(request):
+    return render(request, "mobil.html")
+
+
+def pzrk(request):
+    return render(request, "pzrk.html")
+
+
+def vr(request):
+    return render(request, "vr.html")
